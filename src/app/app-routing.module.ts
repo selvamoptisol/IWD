4ec1305079f91dc974e2abb3293d68982cdef6a4
vanishree165 <vanishree.r@optisolbusiness.com>
2024-01-31 12:08:27 +0530

    router set

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component'
+import { VanishreePortfolioComponent } from './portfolios/vanishree-portfolio/vanishree-portfolio.component';
 
 const routes: Routes = [
   {
     path: '',
-    children: []
+    component: HomeComponent
+  },
+  { path: 'vanishree', 
+    component: VanishreePortfolioComponent 
   }
 ];
 
